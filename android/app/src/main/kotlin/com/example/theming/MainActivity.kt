be91c2f2423c896852814b543ae23df0844ac474
Mohamed Fares <mofares010@gmail.com>
2026-06-10 18:45:54 +0300

    Login and Register UI

---
+++ android/app/src/main/kotlin/com/example/theming/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.theming
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/register.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                'Create Account',
+                style: TextStyle(
+                  color: const Color(0xFF1A242F),
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500,
+                  height: 1.29,
+                ),
+              ),
+            ),
+            SizedBox(height: 4),
+            Center(
+              child: SizedBox(
+                width: 267,
+                child: Text(
+                  'Welcome Back You’ve Been Missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: const Color(0xFF707B81),
+                    fontSize: 16,
+
+                    fontWeight: FontWeight.w400,
+                    height: 1.50,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              'Your Name',
+              style: TextStyle(
+                color: const Color(0xFF1A242F),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                height: 1.25,
+              ),
+            ),
+            SizedBox(height: 6),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Color(0xffFFFFFF),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                  borderRadius: BorderRadius.circular(26),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFE5E7EB)),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+            Text(
+              'Email Address',
+              style: TextStyle(
+                color: const Color(0xFF1A242F),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                height: 1.25,
+              ),
+            ),
+            SizedBox(height: 6),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Color(0xffFFFFFF),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                  borderRadius: BorderRadius.circular(26),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFE5E7EB)),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Password',
+              style: TextStyle(
+                color: const Color(0xFF1A242F),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                height: 1.25,
+              ),
+            ),
+            SizedBox(height: 12),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Color(0xffFFFFFF),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                  borderRadius: BorderRadius.circular(26),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFE5E7EB)),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            Align(
+              alignment: AlignmentGeometry.centerRight,
+              child: Text(
+                'Recovery Password',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  color: const Color(0xFF707B81),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  height: 1.23,
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              width: double.infinity,
+
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 16,
+                ),
+                child: Center(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      height: 1.22,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
